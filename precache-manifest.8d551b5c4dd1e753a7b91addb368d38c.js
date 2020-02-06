9b8ea45d1acd3554caf83a220613d348cba3ba7b
Jongkeun <jongkeun.kr@gmail.com>
2020-02-06 10:57:50 +0900

    Updates

---
+++ precache-manifest.8d551b5c4dd1e753a7b91addb368d38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bc10cfe423e3ac4b4ad006becf011a",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "8a5b469c4f5a76cd6959",
+    "url": "/todo-list-react/static/css/main.f85346bd.chunk.css"
+  },
+  {
+    "revision": "43b6de68cd8f91bf9d4d",
+    "url": "/todo-list-react/static/js/2.d19099b6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-list-react/static/js/2.d19099b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5b469c4f5a76cd6959",
+    "url": "/todo-list-react/static/js/main.b8e0697a.chunk.js"
+  },
+  {
+    "revision": "7f050411dc5a335cf1fa",
+    "url": "/todo-list-react/static/js/runtime-main.f47fe458.js"
+  }
+]);
